ee03d9d18729eb6252efbc8a56b3067980d9056f
hikkosha <hikky93@mail.ru>
2024-09-03 23:28:49 +0300

    03.09

--- src/components/Suras.jsx
+++ src/components/Suras.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import '../css/suras.css';
 
 const Suras = () => {
   return (
     <div className='suras-wrapper'>
       <div className="container">
         <div className="suras-main">
-          
+          <div className="fatiha">
+            <h2 className='nameOfSura'>Fatiha</h2>
+            <span className='separate'></span>
+            <p>بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ</p>
+            <span className='separate'></span>
+            <p>ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَـٰلَمِينَ</p>
+            <span className='separate'></span>
+            <p>ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ</p>
+            <span className='separate'></span>
+            <p>مَـٰلِكِ يَوْمِ ٱلدِّينِ</p>
+            <span className='separate'></span>
+            <p>إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ</p>
+            <span className='separate'></span>
+            <p>ٱهْدِنَا ٱلصِّرَٰطَ ٱلْمُسْتَقِيمَ</p>
+            <span className='separate'></span>
+            <p>عَلَيْهِمْ غَيْرِ ٱلْمَغْضُوبِ عَلَيْهِمْ وَلَا ٱلضَّآلِّينَ</p>
+          </div>
         </div>
       </div>
     </div>
